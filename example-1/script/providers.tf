47b8bfcf6e64089ead944d12bbd591374faf74a0
richard.vlas <vlasrichard@gmail.com>
2023-02-06 06:41:17 +0100

    added example-1

--- example-1/script/providers.tf
+++ example-1/script/providers.tf
@@ -6,10 +6,23 @@ terraform {
             source  = "hashicorp/azurerm"
             version = "=3.41.0"
         }
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~>2.3.2"
+        }
+
     }
 }
 
 # Configures the Microsoft Azure provider.
 provider "azurerm" {
     features {}  
+}
+
+# Configures the Kubernetes (cluster) provider.
+provider "kubernetes" {
+    host                   = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
 }
