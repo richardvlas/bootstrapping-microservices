db19e233c5b75cf1a8fd2c7895d168dcb93fe14c
richard.vlas <vlasrichard@gmail.com>
2023-02-06 06:56:24 +0100

    removed loadbalancer

---
+++ example-3/script/variables.tf
@@ -0,0 +1,21 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+    default = "flixtubervl"
+}
+
+variable location {
+    default = "southeastasia"
+}
+
+variable admin_username {
+    default = "linux_admin"
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
+}
